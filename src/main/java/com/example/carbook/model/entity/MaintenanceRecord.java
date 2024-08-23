ff6157537c2141e4c59334d5e5339616873d21cc
David <danikanisa@gmail.com>
2024-08-23 15:05:37 +0400

    initial commit

--- src/main/java/com/example/carbook/model/entity/MaintenanceRecord.java
+++ src/main/java/com/example/carbook/model/entity/MaintenanceRecord.java
@@ -2,7 +2,6 @@ package com.example.carbook.model.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
